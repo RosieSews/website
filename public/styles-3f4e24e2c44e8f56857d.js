21e376209508b143728455c6f9cedc0b5ed7e6d9
Garrett Van Gotten <gvan09@gmail.com>
2020-03-21 18:55:39 -0500

    add public to the gitignore

--- public/styles-3f4e24e2c44e8f56857d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"+R+8":function(n,o,w){},"0sYV":function(n,o,w){}}]);
-//# sourceMappingURL=styles-3f4e24e2c44e8f56857d.js.map
--- public/styles-4da80ad6dae3878c2773.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{"+R+8":function(n,o,w){},"0sYV":function(n,o,w){}}]);
-//# sourceMappingURL=styles-4da80ad6dae3878c2773.js.map
